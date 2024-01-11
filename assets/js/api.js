1b70285ede45dc67bdd7256ea7229ba373561cb7
Lucas Augusto <77333486+Lucassilva1010@users.noreply.github.com>
2024-01-11 16:34:10 -0300

    mudança da Url do Json

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function buscaDadosPerfil() {
-    const url = 'https://raw.githubusercontent.com/Lucassilva1010/portifolio2024/main/assets/data/profile.json?token=GHSAT0AAAAAACFEOXLG2XCPKPQVT5L2Q2IUZNAH37Q';
+    const url = 'https://raw.githubusercontent.com/Lucassilva1010/portifolio2024/main/assets/data/profile.json?token=GHSAT0AAAAAACFEOXLGB3CE4UUF423BF7UYZNAIK4A';
     const buscaDados = await fetch(url)
     return await buscaDados.json()
 }
