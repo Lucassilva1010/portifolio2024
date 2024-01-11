97552e972cbb9f4e93e33ab4772498774ac2fb5f
Lucas Augusto <77333486+Lucassilva1010@users.noreply.github.com>
2024-01-11 16:19:13 -0300

    alteração das variaveis json

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function buscaDadosPerfil() {
-    const url = 'https://raw.githubusercontent.com/RenanJPaula/js-developer-portfolio/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/Lucassilva1010/portifolio2024/main/assets/data/profile.json?token=GHSAT0AAAAAACFEOXLG2XCPKPQVT5L2Q2IUZNAH37Q';
     const buscaDados = await fetch(url)
     return await buscaDados.json()
 }
